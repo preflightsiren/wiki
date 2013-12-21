1bdc5dcd09de0ab546c27c2a42a8eeac7b392cab
Sebastian Cole <sebastian.paris@gmail.com>
2013-12-21 13:28:38 +1100

    Implementing edit, save functionality

--- wiki.go
+++ wiki.go
@@ -26,9 +26,31 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+  title := r.URL.Path[len("/view/"):]
+  p, _ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/edit/"):]
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{ Title: title }
+  }
+  fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+    "<form action=\"/save/%s\" method=\"POST\">"+
+    "<textarea name=\"body\">%s</textarea><br>"+
+    "<input type=\"submit\" value=\"Save\">"+
+    "</form>",
+    p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/save/"):]
+  body := r.FormValue("body")
+  p := &Page{ Title: title, Body: []byte(body) }
+  p.save()
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
@@ -37,6 +59,8 @@ func main() {
   p2, _ := loadPage("FrontPage")
   fmt.Println(string(p2.Body))
   http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
   http.ListenAndServe(":8080", nil)
 
 }
